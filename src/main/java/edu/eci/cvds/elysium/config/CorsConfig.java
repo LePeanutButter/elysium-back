d836d445fffd42e83e6627eba7784909b5967de8
Santiago Botero <157855016+LePeanutButter@users.noreply.github.com>
2025-03-11 14:36:34 -0500

    Add CORS configuration to project

---
+++ src/main/java/edu/eci/cvds/elysium/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package edu.eci.cvds.elysium.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500") // Cambiar el origen al necesario
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
